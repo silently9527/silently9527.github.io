9573905f91cdbd3f28d1a035df239f0b0fc2d2fc
silently9527 <380303318@qq.com>
2024-08-11 12:25:54 +0000

    Deploying to pages from @ silently9527/silently9527.github.io@977dad4e4ad7776eee2948dc36f390454dad4ffc 🚀

---
+++ assets/article_pamphlet_algorithms_卧学算法_图算法系列之深度优先搜索一.md.C9OjZR4z.js
@@ -0,0 +1,123 @@
+import{_ as t}from"./chunks/ArticleMetadata.IwlMn1X8.js";import{_ as c,D as r,o as p,c as o,I as h,w as d,k as e,a as g,R as m,b as u,e as b}from"./chunks/framework.412fhWlr.js";import"./chunks/md5.6OGdLw3O.js";const I=JSON.parse('{"title":"图算法系列之深度优先搜索（一）","description":"","frontmatter":{"title":"图算法系列之深度优先搜索（一）","author":"Herman","date":"2021/08/14 13:58","categories":["算法"],"tags":["深度优先搜索","图算法"]},"headers":[],"relativePath":"article/pamphlet/algorithms/卧学算法/图算法系列之深度优先搜索一.md","filePath":"article/pamphlet/algorithms/卧学算法/图算法系列之深度优先搜索一.md","lastUpdated":1723379109000}'),f={name:"article/pamphlet/algorithms/卧学算法/图算法系列之深度优先搜索一.md"},v=e("h1",{id:"图算法系列之深度优先搜索-一",tabindex:"-1"},[g("图算法系列之深度优先搜索（一） "),e("a",{class:"header-anchor",href:"#图算法系列之深度优先搜索-一","aria-label":'Permalink to "图算法系列之深度优先搜索（一）"'},"​")],-1),k=m(`<h2 id="前言" tabindex="-1">前言 <a class="header-anchor" href="#前言" aria-label="Permalink to &quot;前言&quot;">​</a></h2><p>在上一篇中我们把图通过邻接表数组表示出来了，这个数据结构将会做我们实现图算法的基础，本篇我们将一起开始学习图算法的第一个搜索算法 - 深度优先搜索</p><h2 id="搜索api的定义" tabindex="-1">搜索API的定义 <a class="header-anchor" href="#搜索api的定义" aria-label="Permalink to &quot;搜索API的定义&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class Search {</span></span>
+<span class="line"><span>    Search(Graph graph, int s);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    boolean marked(int v);</span></span>
+<span class="line"><span>    </span></span>
+<span class="line"><span>    int count();</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>在开始实现算法之前，我们依然先来定义搜索的API</p><ol><li>构造方法提供了一个图对象，以及一个起点s，需要找到与s连通的所有顶点</li><li>marked判断顶点s与v是否相邻</li><li>count返回与顶点s相连的总顶点数</li></ol><h2 id="深度优先搜索" tabindex="-1">深度优先搜索 <a class="header-anchor" href="#深度优先搜索" aria-label="Permalink to &quot;深度优先搜索&quot;">​</a></h2><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/b0e06abdd6f540978af8f3e7a6c00f15~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>假如上图是一个迷宫，我们需要从顶点0开始找到一条出路，假设我们有一条无限长的绳子，和一支粉笔，那么可以这样考虑找到出路：</p><ol><li>先选择一条通道走，在走的路上放上一根绳子</li><li>每遇到一个路口就用笔标记一下，继续选择一条未走过的通道</li><li>当遇到一个已经被标记的路口时就退回到上一个路口继续选择一个未走过的通道</li><li>当回退的路口已经没有路可以走的时候就在继续往后回退</li></ol><p>这种方式绳子总能帮你找到一条出路，而标记不会让你重复走已经走过的通道。</p><p>深度优先搜索的实现思路就和走迷宫的方式一样；</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class DepthFirstSearch {</span></span>
+<span class="line"><span>    private boolean marked[]; </span></span>
+<span class="line"><span>    private int count;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public DepthFirstSearch(Graph graph, int s) {</span></span>
+<span class="line"><span>        this.marked = new boolean[graph.V()];</span></span>
+<span class="line"><span>        this.dfs(graph, s);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private void dfs(Graph graph, int v) {</span></span>
+<span class="line"><span>        marked[v] = true;</span></span>
+<span class="line"><span>        count++;</span></span>
+<span class="line"><span>        for (int w : graph.adj(v)) {</span></span>
+<span class="line"><span>            if (!marked[w]) {</span></span>
+<span class="line"><span>                dfs(graph, w);</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public boolean marked(int v) {</span></span>
+<span class="line"><span>        return marked[v];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public int count() {</span></span>
+<span class="line"><span>        return count;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>在搜索一张图的时候，使用递归来遍历所有的顶点，在访问其中一个顶点时：</p><ol><li>标记它被已经访问</li><li>递归的访问与之相连的所有邻接点</li></ol><p><strong>单元测试：</strong> 构建下面这张图，然后测试深度优先搜索</p><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7c7556f2ba204332b7d920d5b58c71d5~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>@Test</span></span>
+<span class="line"><span>public void test() {</span></span>
+<span class="line"><span>    Graph graph = new Graph(8); //构建一张图</span></span>
+<span class="line"><span>    graph.addEdge(0, 1);</span></span>
+<span class="line"><span>    graph.addEdge(0, 2);</span></span>
+<span class="line"><span>    graph.addEdge(0, 5);</span></span>
+<span class="line"><span>    graph.addEdge(1, 3);</span></span>
+<span class="line"><span>    graph.addEdge(2, 4);</span></span>
+<span class="line"><span>    graph.addEdge(4, 3);</span></span>
+<span class="line"><span>    graph.addEdge(5, 3);</span></span>
+<span class="line"><span>    </span></span>
+<span class="line"><span>    graph.addEdge(6, 7); //为了展示</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    SeDepthFirstSearcharch search = new DepthFirstSearch(graph, 0);</span></span>
+<span class="line"><span>    System.out.println(search.count());</span></span>
+<span class="line"><span>    System.out.println(search.marked(6));</span></span>
+<span class="line"><span>    System.out.println(search.marked(7));</span></span>
+<span class="line"><span>    System.out.println(search.marked(2));</span></span>
+<span class="line"><span>    System.out.println(search.marked(5));</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/322bab4a5e7e45008684ea286e9be503~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><h2 id="寻找路径的api" tabindex="-1">寻找路径的API <a class="header-anchor" href="#寻找路径的api" aria-label="Permalink to &quot;寻找路径的API&quot;">​</a></h2><p>以上的递归算法只是一个开始，从上面的结果我们可以看出，我们只能判断出哪些顶点与起点s是连通的，无法给出具体的路径出来；换句话说，<strong>我们需要实现从顶点s到顶点v是否存在路径可达，如果存在请打印出来</strong></p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class Paths {</span></span>
+<span class="line"><span>    Paths(Graph graph, int s);</span></span>
+<span class="line"><span>    </span></span>
+<span class="line"><span>    boolean hasPathTo(int v); //判断出从s-&gt;v是否存在路径</span></span>
+<span class="line"><span>    </span></span>
+<span class="line"><span>    Iterable&lt;Integer&gt; pathTo(int v); //如果存在路径，返回路径</span></span>
+<span class="line"><span>}</span></span></code></pre></div><h2 id="基于深度优先搜索查找图中的可达路径" tabindex="-1">基于深度优先搜索查找图中的可达路径 <a class="header-anchor" href="#基于深度优先搜索查找图中的可达路径" aria-label="Permalink to &quot;基于深度优先搜索查找图中的可达路径&quot;">​</a></h2><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7d8547f0ac3143939aa2e5ce9ea23277~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>我们依然基于这张图来看，由于我们需要找出可达的路径，所以我们在进行搜索的时候需要记录下图中的边，这里我们使用的是一个数组edgeTo[]，如果存在一条边是v-&gt;w，那么可以表示成edgeTo[w]=v，在深度搜索完成之后这个edgeTo[]数组就是一颗由父链表示的一颗树 （父链树在之前的文章中也使用过<a href="https://juejin.cn/post/6930395454739841037" target="_blank" rel="noreferrer">《如何检测社交网络中两个人是否是朋友关系（union-find算法）》</a>）</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class DepthFirstPaths {</span></span>
+<span class="line"><span>    private boolean marked[];</span></span>
+<span class="line"><span>    private int[] edgeTo;</span></span>
+<span class="line"><span>    private int s;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    DepthFirstPaths(Graph graph, int s) {</span></span>
+<span class="line"><span>        this.s = s;</span></span>
+<span class="line"><span>        this.marked = new boolean[graph.V()];</span></span>
+<span class="line"><span>        this.edgeTo = new int[graph.V()];</span></span>
+<span class="line"><span>        this.dfs(graph, s);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private void dfs(Graph graph, int v) {</span></span>
+<span class="line"><span>        this.marked[v] = true;</span></span>
+<span class="line"><span>        for (int w : graph.adj(v)) {</span></span>
+<span class="line"><span>            if (!marked[w]) {</span></span>
+<span class="line"><span>                this.edgeTo[w] = v;</span></span>
+<span class="line"><span>                this.dfs(graph, w);</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public boolean hasPathTo(int v) {</span></span>
+<span class="line"><span>        return marked[v];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public Iterable&lt;Integer&gt; pathTo(int v) {</span></span>
+<span class="line"><span>        if (!hasPathTo(v)) {</span></span>
+<span class="line"><span>            throw new IllegalStateException(&quot;s不能到达v&quot;);</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        Stack&lt;Integer&gt; stack = new LinkedListStack&lt;&gt;();</span></span>
+<span class="line"><span>        stack.push(v);</span></span>
+<span class="line"><span>        while (edgeTo[v] != s) {</span></span>
+<span class="line"><span>            stack.push(edgeTo[v]);</span></span>
+<span class="line"><span>            v = edgeTo[v];</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        stack.push(s);</span></span>
+<span class="line"><span>        return stack;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>画图来详细跟踪深度优先搜索的运行轨迹，记录了edgeTo的变化以及父链树的逐渐形成</p><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/df521e755a8a4a2ab64e3647bfa37709~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/708c04bce39146e9bf37c1ada3fdeba3~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/80f4afebaec24bfba794dc66fca116de~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>最终父链树形成了，接下来我们来写单元测试校验下生成的父链树和实际的运行结果是否一致</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>@Test</span></span>
+<span class="line"><span>public void test() {</span></span>
+<span class="line"><span>    Graph graph = new Graph(8);</span></span>
+<span class="line"><span>    graph.addEdge(0, 1);</span></span>
+<span class="line"><span>    graph.addEdge(0, 2);</span></span>
+<span class="line"><span>    graph.addEdge(0, 5);</span></span>
+<span class="line"><span>    graph.addEdge(1, 3);</span></span>
+<span class="line"><span>    graph.addEdge(2, 4);</span></span>
+<span class="line"><span>    graph.addEdge(4, 3);</span></span>
+<span class="line"><span>    graph.addEdge(5, 3);</span></span>
+<span class="line"><span>    graph.addEdge(6, 7);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    DepthFirstPaths paths = new DepthFirstPaths(graph, 0);</span></span>
+<span class="line"><span>    System.out.println(paths.hasPathTo(5));</span></span>
+<span class="line"><span>    System.out.println(paths.hasPathTo(2));</span></span>
+<span class="line"><span>    System.out.println(paths.hasPathTo(6));</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    paths.pathTo(5).forEach(System.out::print);</span></span>
+<span class="line"><span>    System.out.println();</span></span>
+<span class="line"><span>    paths.pathTo(4).forEach(System.out::print);</span></span>
+<span class="line"><span>    System.out.println();</span></span>
+<span class="line"><span>    paths.pathTo(2).forEach(System.out::print);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>验证结果完全匹配了父链树</p><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/1cb5b06a5ebc4e318ac4d72e7114106b~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p>`,34);function _(a,y,T,S,P,w){const l=t,i=r("ClientOnly");return p(),o("div",null,[v,h(i,null,{default:d(()=>{var s,n;return[(((s=a.$frontmatter)==null?void 0:s.aside)??!0)&&(((n=a.$frontmatter)==null?void 0:n.showArticleMetadata)??!0)?(p(),u(l,{key:0,article:a.$frontmatter},null,8,["article"])):b("",!0)]}),_:1}),k])}const q=c(f,[["render",_]]);export{I as __pageData,q as default};
---
+++ assets/article_pamphlet_algorithms_卧学算法_图算法系列之深度优先搜索二.md.y4jiSLLR.js
@@ -0,0 +1,129 @@
+import{_ as t}from"./chunks/ArticleMetadata.IwlMn1X8.js";import{_ as c,D as r,o as p,c as o,I as d,w as h,k as e,a as u,R as g,b as m,e as v}from"./chunks/framework.412fhWlr.js";import"./chunks/md5.6OGdLw3O.js";const G=JSON.parse('{"title":"图算法系列之深度优先搜索（二）","description":"","frontmatter":{"title":"图算法系列之深度优先搜索（二）","author":"Herman","date":"2021/08/14 13:58","categories":["算法"],"tags":["深度优先搜索","图算法"]},"headers":[],"relativePath":"article/pamphlet/algorithms/卧学算法/图算法系列之深度优先搜索二.md","filePath":"article/pamphlet/algorithms/卧学算法/图算法系列之深度优先搜索二.md","lastUpdated":1723379109000}'),b={name:"article/pamphlet/algorithms/卧学算法/图算法系列之深度优先搜索二.md"},f=e("h1",{id:"图算法系列之深度优先搜索-二",tabindex:"-1"},[u("图算法系列之深度优先搜索（二） "),e("a",{class:"header-anchor",href:"#图算法系列之深度优先搜索-二","aria-label":'Permalink to "图算法系列之深度优先搜索（二）"'},"​")],-1),k=g(`<p>在上篇中我们学习了深度优先搜索，知道了如何通过深度优先搜索在图中寻找路径；本篇我们继续一起来学习深度优先搜索算法的其他应用场景</p><h2 id="连通分量" tabindex="-1">连通分量 <a class="header-anchor" href="#连通分量" aria-label="Permalink to &quot;连通分量&quot;">​</a></h2><p>从一幅图中找出所有的连通分量，这是也是深度优先搜索的一个应用场景。什么是连通分量？这个定义在之前的文章中已有提到<a href="https://juejin.cn/post/6930395454739841037" target="_blank" rel="noreferrer">《如何检测社交网络中两个人是否是朋友关系（union-find算法）》</a></p><p>在这篇采用的是union-find算法实现的连通性检查，本篇我们将采用深度优先搜索的方式来找出图中的所有连通分量</p><h4 id="连通分量的api定义" tabindex="-1">连通分量的API定义 <a class="header-anchor" href="#连通分量的api定义" aria-label="Permalink to &quot;连通分量的API定义&quot;">​</a></h4><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class DepthFirstCC {</span></span>
+<span class="line"><span>    public DepthFirstCC(Graph graph); </span></span>
+<span class="line"><span>    </span></span>
+<span class="line"><span>    public boolean connected(int v, int w); //检查两个顶点是否连通</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public int count(); //统计连通分量的总数</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public int id(int v); //顶点v所在连通分量的标识</span></span>
+<span class="line"><span>}</span></span></code></pre></div><h4 id="连通分量的api实现" tabindex="-1">连通分量的API实现 <a class="header-anchor" href="#连通分量的api实现" aria-label="Permalink to &quot;连通分量的API实现&quot;">​</a></h4><p>与之前一样没扫描到一个顶点我们就需要标记这个顶点，所以依然需要定义一个marked[]数组</p><p>为了统计出图中总共有多少连通分量，所以需要定义一个变量count</p><p>为了判断两个顶点是否相连，我们需要把相连的顶点对应的标识值记录成相同值，当在调用connected方法的时候直接取出两个顶点的标识值比较，如果相同就是连通的，否则就是非连通；</p><p>这个的标识值我们使用的是count的值，每个顶点都需要存一个标识值，所以还需要一个ids[]数组。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class DepthFirstCC {</span></span>
+<span class="line"><span>    private boolean marked[];</span></span>
+<span class="line"><span>    private int count;</span></span>
+<span class="line"><span>    private int[] ids;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public DepthFirstCC(Graph graph) {</span></span>
+<span class="line"><span>        this.marked = new boolean[graph.V()];</span></span>
+<span class="line"><span>        this.ids = new int[graph.V()];</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>        for (int v = 0; v &lt; graph.V(); v++) {</span></span>
+<span class="line"><span>            if (!this.marked[v]) {</span></span>
+<span class="line"><span>                dfs(graph, v);</span></span>
+<span class="line"><span>                count++;</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private void dfs(Graph graph, int v) {</span></span>
+<span class="line"><span>        this.marked[v] = true;</span></span>
+<span class="line"><span>        this.ids[v] = count;</span></span>
+<span class="line"><span>        for (int w : graph.adj(v)) {</span></span>
+<span class="line"><span>            if (!this.marked[w]) {</span></span>
+<span class="line"><span>                dfs(graph, w);</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public boolean connected(int v, int w) {</span></span>
+<span class="line"><span>        return id(v) == id(w);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public int count() {</span></span>
+<span class="line"><span>        return count;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public int id(int v) {</span></span>
+<span class="line"><span>        return ids[v];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>}</span></span></code></pre></div><h4 id="单元测试" tabindex="-1">单元测试 <a class="header-anchor" href="#单元测试" aria-label="Permalink to &quot;单元测试&quot;">​</a></h4><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/d5dd95fdc26242ffaacd395891193375~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>构造这样一个图，连通分量的总数应该是3</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>@Test</span></span>
+<span class="line"><span>public void test() {</span></span>
+<span class="line"><span>    Graph graph = new Graph(10);</span></span>
+<span class="line"><span>    graph.addEdge(0, 1);</span></span>
+<span class="line"><span>    graph.addEdge(0, 2);</span></span>
+<span class="line"><span>    graph.addEdge(0, 5);</span></span>
+<span class="line"><span>    graph.addEdge(1, 3);</span></span>
+<span class="line"><span>    graph.addEdge(2, 4);</span></span>
+<span class="line"><span>    graph.addEdge(4, 3);</span></span>
+<span class="line"><span>    graph.addEdge(5, 3);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    graph.addEdge(6, 7);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    graph.addEdge(8, 9);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    DepthFirstCC cc = new DepthFirstCC(graph);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    System.out.println(cc.connected(0,5));</span></span>
+<span class="line"><span>    System.out.println(cc.connected(1,2));</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    System.out.println(cc.count());</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/d59c6123ec994ac9b55c65464f561c12~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><blockquote><p>基于深度优先搜索实现的连通性检查理论上说要比以前实现的union-find算法更快，因为检查连通性深度优先搜索实现的版本能够保证在常量时间内完成，而union-find算法不行；</p><p>但是union-find也有自己的优势: 不需要把完整的构造并表示一张图，更重要的是union-find算法是动态的添加节点。</p></blockquote><h2 id="检查无向图中是否有环" tabindex="-1">检查无向图中是否有环 <a class="header-anchor" href="#检查无向图中是否有环" aria-label="Permalink to &quot;检查无向图中是否有环&quot;">​</a></h2><p>为了减小实现的复杂度，我们假设图中不存在自环和平行边；</p><p>假如从顶点v出发存在环，表示从顶点v出发的连通分量中某个顶点的邻接顶点是v，那么在搜索的过程中必定会再次遇到顶点v</p><p>实现的思路：</p><ol><li>标记已经搜索过的每个顶点</li><li>当遇到了一个已经被标记过的顶点，表示已经图中存在环；</li><li>由于图是无向图，如果v-w相连，那么顶点v中的邻接表中有w，w邻接表中也会有v，但是他们没有构成环，所以需要排除掉该情况。</li></ol><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class Cycle {</span></span>
+<span class="line"><span>    private boolean marked[];</span></span>
+<span class="line"><span>    private boolean hashCycle;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public Cycle(Graph graph) {</span></span>
+<span class="line"><span>        this.marked = new boolean[graph.V()];</span></span>
+<span class="line"><span>        for (int s = 0; s &lt; graph.V(); s++) {</span></span>
+<span class="line"><span>            if (!this.marked[s]) {</span></span>
+<span class="line"><span>                dfs(graph, s, s);</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private void dfs(Graph graph, int v, int pV) {</span></span>
+<span class="line"><span>        this.marked[v] = true;</span></span>
+<span class="line"><span>        for (int w : graph.adj(v)) {</span></span>
+<span class="line"><span>            if (!this.marked[w]) {</span></span>
+<span class="line"><span>                this.dfs(graph, w, v);</span></span>
+<span class="line"><span>            } else if (w != pV) {</span></span>
+<span class="line"><span>                this.hashCycle = true;</span></span>
+<span class="line"><span>                return;</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public boolean hasCycle() {</span></span>
+<span class="line"><span>        return hashCycle;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>方法dfs的参数v表示需要待搜索的顶点，pV表示的是到达v的顶点，所以如果v的邻接表中有个顶点已被标记过并且该顶点不等于到达v的顶点，那么表示图中有环</p><h2 id="检查无向图是否是二分图" tabindex="-1">检查无向图是否是二分图 <a class="header-anchor" href="#检查无向图是否是二分图" aria-label="Permalink to &quot;检查无向图是否是二分图&quot;">​</a></h2><p>何为二分图? 图中每条边所连接的顶点都属于不同的部分；如下图：</p><p><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/e41927a074134d0c8782416244bef414~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>其中红色节点表示一个集合，白色节点是另一个集合，每条边连接的两个顶点属于不同的集合；</p><p>举个实际的例子就很好理解，电影与演员的关系，电影作为一个顶点，演员作为一个顶点，电影与电影直接是不会有边，演员与演员直接也不会有边，这就是一张二分图。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark-dimmed vp-code"><code><span class="line"><span>public class TwoColorGraph {</span></span>
+<span class="line"><span>    private boolean twoColor = true;</span></span>
+<span class="line"><span>    private boolean[] marked;</span></span>
+<span class="line"><span>    private boolean[] color;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public TwoColorGraph(Graph graph) {</span></span>
+<span class="line"><span>        this.marked = new boolean[graph.V()];</span></span>
+<span class="line"><span>        this.color = new boolean[graph.V()];</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>        for (int v = 0; v &lt; graph.V(); v++) {</span></span>
+<span class="line"><span>            if (!this.marked[v]) {</span></span>
+<span class="line"><span>                dfs(graph, v);</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private void dfs(Graph graph, int v) {</span></span>
+<span class="line"><span>        this.marked[v] = true;</span></span>
+<span class="line"><span>        for (int w : graph.adj(v)) {</span></span>
+<span class="line"><span>            if (!this.marked[w]) {</span></span>
+<span class="line"><span>                this.color[w] = !this.color[v];</span></span>
+<span class="line"><span>                dfs(graph, w);</span></span>
+<span class="line"><span>            } else if (this.color[w] == this.color[v]) {</span></span>
+<span class="line"><span>                this.twoColor = false;</span></span>
+<span class="line"><span>                return;</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public boolean isTwoColor() {</span></span>
+<span class="line"><span>        return twoColor;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div>`,31);function _(s,C,w,y,V,P){const l=t,i=r("ClientOnly");return p(),o("div",null,[f,d(i,null,{default:h(()=>{var n,a;return[(((n=s.$frontmatter)==null?void 0:n.aside)??!0)&&(((a=s.$frontmatter)==null?void 0:a.showArticleMetadata)??!0)?(p(),m(l,{key:0,article:s.$frontmatter},null,8,["article"])):v("",!0)]}),_:1}),k])}const T=c(b,[["render",_]]);export{G as __pageData,T as default};
