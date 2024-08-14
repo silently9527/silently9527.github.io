570adbe1ddb3c41b979969afcf6d6c5dc6ad8822
Herman <380303318@qq.com>
2024-08-14 17:13:33 +0800

    init

--- docs/.vitepress/components/Comment.vue
+++ docs/.vitepress/components/Comment.vue
@@ -37,7 +37,6 @@
       distractionFreeMode: false,
       updateCountCallback: function(count) {
         var commentToPin = $('.gt-comment-admin').last();
-        console.log(commentToPin)
         if (commentToPin.length) {
           // 将该评论元素插入到评论区的最顶部
           var commentsContainer = $('.gt-comments>div');
