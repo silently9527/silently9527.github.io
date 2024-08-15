b09ea5195a23a5c3d6c0c60cfb920942d984c622
Herman <380303318@qq.com>
2024-08-15 10:58:37 +0800

    init

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -11,8 +11,29 @@ import HText from "../components/HText.vue";
 export default {
   extends: DefaultTheme,
   Layout: MyLayout,
-  enhanceApp(ctx) {
-    ctx.app.component("LinkCard", LinkCard);
-    ctx.app.component("HText", HText);
+  enhanceApp({ app, router }) {
+    app.component("LinkCard", LinkCard);
+    app.component("HText", HText);
+
+    // 这里插入百度统计代码
+    if (typeof window !== 'undefined') {
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?e8f29d251188c407b607df359432a40c";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+
+
+      router.onAfterRouteChanged = (to, from) => {
+        if (typeof _hmt !== "undefined") {
+          if (to.path) {
+            _hmt.push(["_trackPageview", to.fullPath]);
+          }
+        }
+      }
+    }
+
   },
 };
