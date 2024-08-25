6241f854d874f19b022f1a4c2d696235bf08eb84
silently9527 <380303318@qq.com>
2024-08-25 16:07:38 +0000

    Deploying to pages from @ silently9527/silently9527.github.io@25fcfad5ccb29767d53100a015fda07a38a355c8 🚀

---
+++ assets/Notes_No2TechColumn_04 Algorithms_03 如何检测社交网络中两个人是否是朋友关系（union-find算法）.md.4Dj3IDka.js
@@ -0,0 +1,120 @@
+import{_ as n,c as s,o as a,aa as p}from"./chunks/framework.DOnJscRE.js";const q=JSON.parse('{"title":"03 union-find算法","description":"","frontmatter":{"title":"03 union-find算法","author":"Herman","updateTime":"2024-07-16 21:34","desc":"如何检测社交网络中两个人是否是朋友关系","categories":"算法","tags":"数据结构/union-find算法","outline":"deep"},"headers":[],"relativePath":"Notes/No2TechColumn/04 Algorithms/03 如何检测社交网络中两个人是否是朋友关系（union-find算法）.md","filePath":"Notes/No2TechColumn/04 Algorithms/03 如何检测社交网络中两个人是否是朋友关系（union-find算法）.md","lastUpdated":1724595807000}'),i={name:"Notes/No2TechColumn/04 Algorithms/03 如何检测社交网络中两个人是否是朋友关系（union-find算法）.md"},l=p(`<h1 id="如何检测社交网络中两个人是否是朋友关系" tabindex="-1">如何检测社交网络中两个人是否是朋友关系 <a class="header-anchor" href="#如何检测社交网络中两个人是否是朋友关系" aria-label="Permalink to &quot;如何检测社交网络中两个人是否是朋友关系&quot;">​</a></h1><h2 id="前言" tabindex="-1">前言 <a class="header-anchor" href="#前言" aria-label="Permalink to &quot;前言&quot;">​</a></h2><p>春节放假会了老家，停更了很多天，这是年后连夜肝出来的第一篇文章，先来聊聊春节放假期间发生的事，这次回家遇到了我学生时代的女神，当年她在我心目中那是</p><blockquote><p>&quot;出淤泥而不染、濯清涟而不妖&quot;</p></blockquote><p>没想到这次回家遇到了她，身体发福了，心目中女神的形象瞬间碎了，就好像达芬奇再次遇到了蒙娜丽莎</p><blockquote><p>&quot;菡萏香销翠叶残&quot;</p></blockquote><p>好了，言归正传。</p><p>有时候我们可以需要判断在大型网络中两台计算机是否相连，是否需要建立一条新的连接才能通信；或者是在社交网络中判断两个人是否是朋友关系（相连表示是朋友关系）。在这种应用中，通常我们可能需要处理数百万的对象和数亿的连接，如何能够快速的判断出是否相连呢？这就需要使用到union-find算法</p><h2 id="概念" tabindex="-1">概念 <a class="header-anchor" href="#概念" aria-label="Permalink to &quot;概念&quot;">​</a></h2><h4 id="相连" tabindex="-1">相连 <a class="header-anchor" href="#相连" aria-label="Permalink to &quot;相连&quot;">​</a></h4><p>假如输入一对整数，其中每个数字表示的是某种对象（人、地址或者计算机等等），整数对p,q理解为“p与q相连”，相连具有以下特性：</p><ul><li>自反性：p与p是相连的</li><li>对称性：如果p与q相连，那么q与p相连</li><li>传递性：如果p与q相连，q与r相连，那么p与r也相连</li></ul><blockquote><p>对象如何与数字关联起来，后面我们聊到一种算法符号表</p></blockquote><h4 id="等价类" tabindex="-1">等价类 <a class="header-anchor" href="#等价类" aria-label="Permalink to &quot;等价类&quot;">​</a></h4><p>假设相连是一个种等价关系，那么等价关系能够将对象划分为多个等价类，在该算法中，当且仅当两个对象相连时他们才属于同一个等价类</p><h4 id="触点" tabindex="-1">触点 <a class="header-anchor" href="#触点" aria-label="Permalink to &quot;触点&quot;">​</a></h4><p>整个网络中的某种对象称为触点</p><h4 id="连通分量" tabindex="-1">连通分量 <a class="header-anchor" href="#连通分量" aria-label="Permalink to &quot;连通分量&quot;">​</a></h4><p>将整数对称为连接，将等价类称作连通分量或者简称分量</p><p><img src="https://cdn.jsdelivr.net/gh/silently9527/images/51cd832609504204877bdcf3b52e0292~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><h4 id="动态连通性" tabindex="-1">动态连通性 <a class="header-anchor" href="#动态连通性" aria-label="Permalink to &quot;动态连通性&quot;">​</a></h4><p>union-find算法的目标是当程序从输入中读取了整数对p q时，如果已知的所有整数对都不能说明p q是相连的，那么将这一对整数输出，否则忽略掉这对整数；我们需要设计数据结构来保存已知的所有整数对的信息，判断出输入的整数对是否是相连的，这种问题叫做动态连通性问题。</p><h2 id="union-find算法api定义" tabindex="-1">union-find算法API定义 <a class="header-anchor" href="#union-find算法api定义" aria-label="Permalink to &quot;union-find算法API定义&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public interface UF {</span></span>
+<span class="line"><span>    void union(int p, int q); //在p与q之间添加一条连接</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    int find(int p); //返回p所在分量的标识符</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    boolean connected(int p, int q); //判断出p与q是否存在于同一个分量中</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    int count(); //统计出连通分量的数量</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>如果两个触点在不同的分量中，union操作会使两个分量归并。一开始我们有N个分量（每个触点表示一个分量），将两个分量归并之后数量减一。</p><p>抽象实现如下：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public abstract class AbstractUF implements UF {</span></span>
+<span class="line"><span>    protected int[] id;</span></span>
+<span class="line"><span>    protected int count;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public AbstractUF(int N) {</span></span>
+<span class="line"><span>        count = N;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>        id = new int[N];</span></span>
+<span class="line"><span>        for (int i = 0; i &lt; N; i++) {</span></span>
+<span class="line"><span>            id[i] = i;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public boolean connected(int p, int q) {</span></span>
+<span class="line"><span>        return find(p) == find(q);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public int count() {</span></span>
+<span class="line"><span>        return count;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>接下来我们就主要来讨论如何实现union方法和find方法</p><h2 id="quick-find算法" tabindex="-1">quick-find算法 <a class="header-anchor" href="#quick-find算法" aria-label="Permalink to &quot;quick-find算法&quot;">​</a></h2><p>这种算法的实现思路是在同一个连通分量中所有触点在id[]中的值都是相同的，判断是否连通的connected的方法就是判断id[p]是否等于id[q]。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public class QuickFindImpl extends AbstractUF {</span></span>
+<span class="line"><span>    public QuickFindImpl(int N) {</span></span>
+<span class="line"><span>        super(N);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public int find(int p) {</span></span>
+<span class="line"><span>        return id[p];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public void union(int p, int q) {</span></span>
+<span class="line"><span>        int pId = find(p);</span></span>
+<span class="line"><span>        int qId = find(q);</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>        if (pId == qId) { //如果相等表示p与q已经属于同一分量中</span></span>
+<span class="line"><span>            return;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>        for (int i = 0; i &lt; id.length; i++) {</span></span>
+<span class="line"><span>            if (id[i] == pId) {</span></span>
+<span class="line"><span>                id[i] = qId; //把分量中所有的值都统一成qId</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        count--; //连通分量数减一</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>}</span></span></code></pre></div><ul><li>算法分析： find()操作显然是很快的，时间复杂度O(1), 但是union的算法是无法处理大型数据的，因为每次都需要变量整个数组，那么union方法的时间复杂度是O(n)</li></ul><h2 id="quick-union算法" tabindex="-1">quick-union算法 <a class="header-anchor" href="#quick-union算法" aria-label="Permalink to &quot;quick-union算法&quot;">​</a></h2><p>为了提高union方法的速度，我们需要考虑另外一种算法；使用同样的数据结构，只是重新定义id[]表示的意义，每个触点所对应的id[]值都是在同一分量中的另一个触点的名称</p><p><img src="https://cdn.jsdelivr.net/gh/silently9527/images/8f670253330f40c0a814047c7d5807fb~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>在数组初始化之后，每个节点的链接都指向自己；id[]数组用<code>父链接</code>的形式表示了<code>森林</code>，每一次union操作都会找出每个分量的<code>根节点</code>进行归并。</p><p><img src="https://cdn.jsdelivr.net/gh/silently9527/images/433b359264384730971733bf2959f22a~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public class QuickUnionImpl extends AbstractUF {</span></span>
+<span class="line"><span>    public QuickUnionImpl(int N) {</span></span>
+<span class="line"><span>        super(N);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public int find(int p) {</span></span>
+<span class="line"><span>        //找出p所在分量的根触点</span></span>
+<span class="line"><span>        while (p != id[p]) {</span></span>
+<span class="line"><span>            p = id[p];</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        return p;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public void union(int p, int q) {</span></span>
+<span class="line"><span>        int pRoot = find(p); //找出q p的根触点</span></span>
+<span class="line"><span>        int qRoot = find(q);</span></span>
+<span class="line"><span>        if (pRoot == qRoot) { //处于同一分量不做处理</span></span>
+<span class="line"><span>            return;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        id[pRoot] = qRoot; //根节点</span></span>
+<span class="line"><span>        count--;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>}</span></span></code></pre></div><ul><li>算法分析： 看起来quick-union算法比quick-find算法更快，因为union不需要为每对输入遍历整个数组， 考虑最佳情况下，find方法只需要访问一次数组就可以得到根触点，那么union方法的时间复杂度O(n)； 考虑到最糟糕的输入情况，如下图：</li></ul><p><img src="https://cdn.jsdelivr.net/gh/silently9527/images/17496e49c2a94903a42501b0d99e1ba2~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>find方法需要访问数组n-1次，那么union方法的时间复杂度是O(n²)</p><h2 id="加权quick-union算法" tabindex="-1">加权quick-union算法 <a class="header-anchor" href="#加权quick-union算法" aria-label="Permalink to &quot;加权quick-union算法&quot;">​</a></h2><p>为了保证quick-union算法最糟糕的情况不在出现，我需要记录每一个树的大小，在进行分量归并操作时总是把小的树连接到大的树上，这种算法构造出来树的高度会远远小于未加权版本所构造的树高度。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public class WeightedQuickUnionImpl extends AbstractUF {</span></span>
+<span class="line"><span>    private int[] sz;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public WeightedQuickUnionImpl(int N) {</span></span>
+<span class="line"><span>        super(N);</span></span>
+<span class="line"><span>        sz = new int[N];</span></span>
+<span class="line"><span>        for (int i = 0; i &lt; N; i++) {</span></span>
+<span class="line"><span>            sz[i] = 1;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public void union(int p, int q) {</span></span>
+<span class="line"><span>        int pRoot = find(p); //找出q p的根触点</span></span>
+<span class="line"><span>        int qRoot = find(q);</span></span>
+<span class="line"><span>        if (pRoot == qRoot) { //处于同一分量不做处理</span></span>
+<span class="line"><span>            return;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        //小树合并到大树</span></span>
+<span class="line"><span>        if (sz[pRoot] &lt; sz[qRoot]) {</span></span>
+<span class="line"><span>            sz[qRoot] += sz[pRoot]; </span></span>
+<span class="line"><span>            id[pRoot] = qRoot;</span></span>
+<span class="line"><span>        } else {</span></span>
+<span class="line"><span>            sz[pRoot] += sz[qRoot];</span></span>
+<span class="line"><span>            id[qRoot] = pRoot;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        count--;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public int find(int p) {</span></span>
+<span class="line"><span>        //找出p所在分量的根触点</span></span>
+<span class="line"><span>        while (p != id[p]) {</span></span>
+<span class="line"><span>            p = id[p];</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        return p;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><ul><li>算法分析： 最坏的情况下，每次union归并的树都是大小相等的，他们都包含了2的n次方个节点，高度都是n，合并之后的高度变成了n+1，由此可以得出union方法的时间复杂度是O(lgN)</li></ul><p><img src="https://cdn.jsdelivr.net/gh/silently9527/images/8c67501200314cc1855255eff1b2c2c1~tplv-k3u1fbpfcp-zoom-1.image" alt=""></p><h2 id="总结" tabindex="-1">总结 <a class="header-anchor" href="#总结" aria-label="Permalink to &quot;总结&quot;">​</a></h2><p>union-find算法只能判断出给定的两个整数是否是相连的，无法给出具体达到的路径；后期我们聊到图算法可以给出具体的路径</p><table><thead><tr><th>算法</th><th>union()</th><th>find()</th></tr></thead><tbody><tr><td>quick-find算法</td><td>N</td><td>1</td></tr><tr><td>quick-union算法</td><td>树的高度</td><td>树的高度</td></tr><tr><td>加权quick-union算法</td><td>lgN</td><td>lgN</td></tr></tbody></table><hr><blockquote><p>参考书籍：算法第四版</p></blockquote>`,51),e=[l];function t(c,o,d,u,r,h){return a(),s("div",null,e)}const f=n(i,[["render",t]]);export{q as __pageData,f as default};
---
+++ assets/Notes_No2TechColumn_04 Algorithms_07 原来实现优先级队列如此简单.md.D_k6gMpD.js
@@ -0,0 +1,124 @@
+import{_ as s,c as a,o as n,aa as p}from"./chunks/framework.DOnJscRE.js";const m=JSON.parse('{"title":"07 原来实现优先级队列如此简单","description":"","frontmatter":{"title":"07 原来实现优先级队列如此简单","author":"Herman","updateTime":"2024-07-16 21:34","desc":"原来实现优先级队列如此简单","categories":"算法","tags":"数据结构/二叉堆","outline":"deep"},"headers":[],"relativePath":"Notes/No2TechColumn/04 Algorithms/07 原来实现优先级队列如此简单.md","filePath":"Notes/No2TechColumn/04 Algorithms/07 原来实现优先级队列如此简单.md","lastUpdated":1724595807000}'),e={name:"Notes/No2TechColumn/04 Algorithms/07 原来实现优先级队列如此简单.md"},l=p(`<h1 id="原来实现优先级队列如此简单" tabindex="-1">原来实现优先级队列如此简单 <a class="header-anchor" href="#原来实现优先级队列如此简单" aria-label="Permalink to &quot;原来实现优先级队列如此简单&quot;">​</a></h1><h2 id="前言" tabindex="-1">前言 <a class="header-anchor" href="#前言" aria-label="Permalink to &quot;前言&quot;">​</a></h2><p>假如你设计的事件系统中有很多的事件，每个事件都定义了不同的权重值，系统需要优先处理权重较高的事件，这里你就需要使用到优先级队列，本篇我们一起来学习实现优先级队列的常用方式</p><h2 id="队列api定义" tabindex="-1">队列API定义 <a class="header-anchor" href="#队列api定义" aria-label="Permalink to &quot;队列API定义&quot;">​</a></h2><p>在实现之前，首先我们需要先定义出优先级队的API，优先级队列是一种抽象的数据结构，我们依然可以基于前面我们使用到的队列API来修改；需要了解之前的队列的实现可以查看《面试的季节到了，老哥确定不来复习下数据结构吗》</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public interface Queue&lt;T&gt; extends Iterable&lt;T&gt; {</span></span>
+<span class="line"><span>    void enqueue(T item); //入队列</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    T dequeue(); //出队列</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    int size();</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    boolean isEmpty();</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>其中的入队列<code>enqueue</code>和出队列<code>dequeue</code>是我们主要需要实现的方式，也是优先级队列的核心方法</p><h2 id="初级版本的实现" tabindex="-1">初级版本的实现 <a class="header-anchor" href="#初级版本的实现" aria-label="Permalink to &quot;初级版本的实现&quot;">​</a></h2><h4 id="队列api的抽象类" tabindex="-1">队列API的抽象类 <a class="header-anchor" href="#队列api的抽象类" aria-label="Permalink to &quot;队列API的抽象类&quot;">​</a></h4><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public abstract class AbstractQueue&lt;T&gt; implements Queue&lt;T&gt; {</span></span>
+<span class="line"><span>    private Comparator&lt;T&gt; comparator;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public AbstractQueue(Comparator&lt;T&gt; comparator) {</span></span>
+<span class="line"><span>        this.comparator = comparator;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public boolean less(T a, T b) {</span></span>
+<span class="line"><span>        return comparator.compare(a, b) &lt; 0;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public void exch(T[] array, int i, int j) {</span></span>
+<span class="line"><span>        T tmp = array[i];</span></span>
+<span class="line"><span>        array[i] = array[j];</span></span>
+<span class="line"><span>        array[j] = tmp;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><h4 id="基于无序数组实现" tabindex="-1">基于无序数组实现 <a class="header-anchor" href="#基于无序数组实现" aria-label="Permalink to &quot;基于无序数组实现&quot;">​</a></h4><p>实现优先级队列的最简单实现可以参考《面试的季节到了，老哥确定不来复习下数据结构吗》中栈的实现方式，<code>enqueue</code>和栈的<code>push</code>方式实现方式一致，<code>dequeue</code>可以参考选择排序的实现，循环一遍数组，找出最大值和数组最后一个元素交换，然后删除它；</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public class DisorderPriorityQueue&lt;T&gt; extends AbstractQueue&lt;T&gt; {</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private T[] queue;</span></span>
+<span class="line"><span>    private int size;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public DisorderPriorityQueue(int max, Comparator&lt;T&gt; comparator) {</span></span>
+<span class="line"><span>        super(comparator);</span></span>
+<span class="line"><span>        this.queue = (T[]) new Object[max];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public void enqueue(T item) {</span></span>
+<span class="line"><span>        queue[size++] = item;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public T dequeue() {</span></span>
+<span class="line"><span>        int index = 0;</span></span>
+<span class="line"><span>        for (int i = 1; i &lt; size; i++) {</span></span>
+<span class="line"><span>            if (less(queue[index], queue[i])) {</span></span>
+<span class="line"><span>                index = i;</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        size--;</span></span>
+<span class="line"><span>        exch(queue, index, size);</span></span>
+<span class="line"><span>        T data = queue[size];</span></span>
+<span class="line"><span>        queue[size] = null;</span></span>
+<span class="line"><span>        return data;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>    //省略其他函数</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>这里只实现了定长的优先级队列，如何实现自动扩容呢？也可以参考这篇文章《面试的季节到了，老哥确定不来复习下数据结构吗》；基于无序数组实现的enqueue时间复杂度是O(1)，dequeue时间复杂度是O(n)</p><h4 id="基于有序数组实现" tabindex="-1">基于有序数组实现 <a class="header-anchor" href="#基于有序数组实现" aria-label="Permalink to &quot;基于有序数组实现&quot;">​</a></h4><p>基于有序数组实现就是在入队的时候保证数组有序，那么在出队列的时候可以直接删掉最大值；插入的过程和插入排序类似的操作</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public class OrderPriorityQueue&lt;T&gt; extends AbstractQueue&lt;T&gt; {</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    private T[] queue;</span></span>
+<span class="line"><span>    private int size;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public OrderPriorityQueue(int max, Comparator&lt;T&gt; comparator) {</span></span>
+<span class="line"><span>        super(comparator);</span></span>
+<span class="line"><span>        this.queue = (T[]) new Object[max];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public void enqueue(T item) {</span></span>
+<span class="line"><span>        queue[size++] = item;</span></span>
+<span class="line"><span>        for (int i = size - 1; i &gt; 1 &amp;&amp; less(queue[i], queue[i - 1]); i--) {</span></span>
+<span class="line"><span>            exch(queue, i, i - 1);</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public T dequeue() {</span></span>
+<span class="line"><span>        size--;</span></span>
+<span class="line"><span>        T data = queue[size];</span></span>
+<span class="line"><span>        queue[size] = null;</span></span>
+<span class="line"><span>        return data;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    //省略其他函数</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>enqueue时间复杂度是O(n)，dequeue时间复杂度是O(1)</p><h4 id="基于链表实现" tabindex="-1">基于链表实现 <a class="header-anchor" href="#基于链表实现" aria-label="Permalink to &quot;基于链表实现&quot;">​</a></h4><p>基于链表的实现与上面的类似，有兴趣的可以自己实现</p><blockquote><p>在《面试的季节到了，老哥确定不来复习下数据结构吗》中我们实现的栈和队列的操作都能够在常数时间内完成，但是优先级队列从上面的实现过程，我们发现初级版本的实现插入或删除最大值的操作最糟糕的情况会是线性时间。</p></blockquote><h2 id="二叉堆实现" tabindex="-1">二叉堆实现 <a class="header-anchor" href="#二叉堆实现" aria-label="Permalink to &quot;二叉堆实现&quot;">​</a></h2><h4 id="二叉堆的定义" tabindex="-1">二叉堆的定义 <a class="header-anchor" href="#二叉堆的定义" aria-label="Permalink to &quot;二叉堆的定义&quot;">​</a></h4><p>在二叉堆中，每个节点都将大于等于它的子节点，也成为堆有序；其中根节点是最大的节点。</p><p>二叉堆的表示：</p><p><img src="https://cdn.jsdelivr.net/gh/silently9527/images/b98dc1760a6e407892ac6af85cc436d2%7Etplv-k3u1fbpfcp-zoom-1.image" alt=""></p><p>重点： 在一个二叉堆中，位置k节点的父节点的位置为<code>k/2</code>,它的两个子节点的位置为<code>2k</code>和<code>2k+1</code>; 基于这点，我们可以用数组来表示二叉堆，通过移动数组的下标来找到节点父节点和子节点</p><p>在元素进行插入和删除操作的过程中，会破坏堆有序，所以我们需要做一些操作来保证堆再次有序；主要有两种情况，当某个节点的优先级上升，我们需要<strong>由下向上恢复堆有序（下沉）</strong>；当某个节点优先级下降，我们需要<strong>由上向下恢复堆有序（上浮）</strong></p><h4 id="由上向下恢复堆有序-上浮" tabindex="-1">由上向下恢复堆有序（上浮） <a class="header-anchor" href="#由上向下恢复堆有序-上浮" aria-label="Permalink to &quot;由上向下恢复堆有序（上浮）&quot;">​</a></h4><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>private void swim(int k) {</span></span>
+<span class="line"><span>    while (k &gt; 0 &amp;&amp; less(queue[k / 2], queue[k])) {</span></span>
+<span class="line"><span>        exch(queue, k / 2, k);</span></span>
+<span class="line"><span>        k = k / 2;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>根据当前的节点k找到父节点的位置k/2，比较当前节点和父节点，如果比父节点大就交换，直到找个比当前节点大的父节点或者已上浮到了根节点</p><h4 id="由下向上恢复堆有序-下沉" tabindex="-1">由下向上恢复堆有序（下沉） <a class="header-anchor" href="#由下向上恢复堆有序-下沉" aria-label="Permalink to &quot;由下向上恢复堆有序（下沉）&quot;">​</a></h4><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>private void sink(int k) {</span></span>
+<span class="line"><span>    while (2 * k &lt;= size) {</span></span>
+<span class="line"><span>        int i = 2 * k;</span></span>
+<span class="line"><span>        if (less(queue[i], queue[i + 1])) {</span></span>
+<span class="line"><span>            i++;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        if (less(queue[i], queue[k])) {</span></span>
+<span class="line"><span>            break;</span></span>
+<span class="line"><span>        }</span></span>
+<span class="line"><span>        exch(queue, i, k);</span></span>
+<span class="line"><span>        k = i;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>}</span></span></code></pre></div><h4 id="二叉堆实现优先级队列" tabindex="-1">二叉堆实现优先级队列 <a class="header-anchor" href="#二叉堆实现优先级队列" aria-label="Permalink to &quot;二叉堆实现优先级队列&quot;">​</a></h4><ul><li>入队操作：将新的元素添加到数组末尾，让新元素上浮到适合位置，增加堆的大小</li><li>出队操作：将最大的根节点删除，然后把最后一个元素放入到顶端，下层顶端元素到合适位置，减小堆大小</li></ul><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>public class BinaryHeapPriorityQueue&lt;T&gt; extends AbstractQueue&lt;T&gt; {</span></span>
+<span class="line"><span>    private T[] queue;</span></span>
+<span class="line"><span>    private int size;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    public BinaryHeapPriorityQueue(int max, Comparator&lt;T&gt; comparator) {</span></span>
+<span class="line"><span>        super(comparator);</span></span>
+<span class="line"><span>        this.queue = (T[]) new Object[max + 1];</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public void enqueue(T item) {</span></span>
+<span class="line"><span>        this.queue[++size] = item;</span></span>
+<span class="line"><span>        this.swim(size);</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>    @Override</span></span>
+<span class="line"><span>    public T dequeue() {</span></span>
+<span class="line"><span>        T max = this.queue[1];</span></span>
+<span class="line"><span>        exch(this.queue, 1, size--);</span></span>
+<span class="line"><span>        this.queue[size + 1] = null; //释放内存</span></span>
+<span class="line"><span>        this.sink(1);</span></span>
+<span class="line"><span>        return max;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>    </span></span>
+<span class="line"><span>     //省略其他函数</span></span>
+<span class="line"><span>}</span></span></code></pre></div><blockquote><p>注意：</p><p>由于我们为了方便计算父节点和子节点的索引位置，所以数组中的第一个位置是不会使用的；可以自己思考下使用第一个位置，那么子节点和父节点的位置应该如何计算？</p><p>基于堆的实现，入队和出队的时间复杂对都是logN，解决了初级版本实现的问题。</p><p>数组大小动态扩容和缩容依然可以参考之前栈的实现方式</p></blockquote>`,37),i=[l];function t(c,o,r,u,d,h){return n(),a("div",null,i)}const q=s(e,[["render",t]]);export{m as __pageData,q as default};
